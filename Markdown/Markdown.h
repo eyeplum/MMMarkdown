509501c9701002ed8b3782bed73c88ef2428f884
Yan Li <eyeplum@gmail.com>
2015-06-03 00:54:59 +0800

    Fix framework header

---
+++ Markdown/Markdown.h
@@ -0,0 +1,24 @@
+//
+//  Markdown.h
+//  Markdown
+//
+//  Created by Yan Li on 6/3/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Markdown.
+FOUNDATION_EXPORT double MarkdownVersionNumber;
+
+//! Project version string for Markdown.
+FOUNDATION_EXPORT const unsigned char MarkdownVersionString[];
+
+#import <Markdown/MMMarkdown.h>
+#import <Markdown/MMDocument.h>
+#import <Markdown/MMElement.h>
+#import <Markdown/MMGenerator.h>
+#import <Markdown/MMHTMLParser.h>
+#import <Markdown/MMParser.h>
+#import <Markdown/MMScanner.h>
+#import <Markdown/MMSpanParser.h>
