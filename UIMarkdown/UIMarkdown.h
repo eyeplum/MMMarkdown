64f15991540e17e7ef3ce13844c84c107b878713
Yan Li <eyeplum@gmail.com>
2015-06-11 17:04:23 -0700

    Add target for iOS

---
+++ UIMarkdown/UIMarkdown.h
@@ -0,0 +1,24 @@
+//
+//  UIMarkdown.h
+//  UIMarkdown
+//
+//  Created by Yan Li on 6/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIMarkdown.
+FOUNDATION_EXPORT double UIMarkdownVersionNumber;
+
+//! Project version string for UIMarkdown.
+FOUNDATION_EXPORT const unsigned char UIMarkdownVersionString[];
+
+#import <UIMarkdown/MMMarkdown.h>
+#import <UIMarkdown/MMDocument.h>
+#import <UIMarkdown/MMElement.h>
+#import <UIMarkdown/MMGenerator.h>
+#import <UIMarkdown/MMHTMLParser.h>
+#import <UIMarkdown/MMParser.h>
+#import <UIMarkdown/MMScanner.h>
+#import <UIMarkdown/MMSpanParser.h>
